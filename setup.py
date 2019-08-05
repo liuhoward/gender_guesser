14e58ea3ac9e92d9b25fd303c077dacf666b4c37
liuhoward <liuhoward09@gmail.com>
2019-08-05 16:34:31 -0700

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 from io import open
 import sys
 
@@ -10,15 +9,7 @@ import sys
 setup(
     name='gender_guesser',
     version='0.1',
-    author='Israel Saeta Pérez',
-    author_email='israel@lead-ratings.com',
     packages=['gender_guesser'],
-    package_dir={'gender_guesser': 'gender_guesser'},
-    package_data={'gender_guesser': ['data/*.txt']},
-    test_suite='test',
-    tests_require=['tox'],
-    cmdclass = {'test': Tox},
-    url='https://github.com/lead-ratings/gender-guesser',
     license='GPLv3',
     description='Get the gender from first name.',
 )
