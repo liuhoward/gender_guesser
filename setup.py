fce9d54e1ea4ae90d39f8f9c920c6723f9415a5e
Howard Liu <liuhoward@users.noreply.github.com>
2019-08-19 10:39:26 -0700

    update setup.py add data

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     name='gender_guesser',
     version='0.1',
     packages=['gender_guesser'],
+    package_dir={'gender_guesser': 'gender_guesser'},
+    package_data={'gender_guesser': ['data/*']},
     license='GPLv3',
     description='Get the gender from first name.',
 )
